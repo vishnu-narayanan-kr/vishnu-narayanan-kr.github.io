987ae542bb1a75979a9f1b390c06942272f1d141
vishnu777aero <vishnu777aero@gmail.com>
2020-10-31 02:18:13 +0530

    create home page link in second page

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,5 +1,10 @@
-export default function FirstPost() {
+import Link from 'next/link';
+
+export default function FirstPost() {  
   return (
+    <>
     <h1>First Post!</h1>
+    <p>Go to <Link href='/'><a>Home page</a></Link></p>
+    </>
   )
 }
