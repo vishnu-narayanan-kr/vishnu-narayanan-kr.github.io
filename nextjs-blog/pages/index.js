0c123cd0c1e96dd8c03f5df4c11e4affae4f37a0
vishnu777aero <vishnu777aero@gmail.com>
2020-10-31 02:15:48 +0530

    create first link

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Goto <Link href='/posts/first-post'><a>first page</a></Link>
         </p>
 
         <div className="grid">
